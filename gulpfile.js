eac060a54e33e5cbbaaf98f6ec76b9ff1167e636
Cristina <cristin@gmail.com>
2024-02-22 20:53:56 +0100

    Subir a GitHub

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,45 @@
-const gulp = require('gulp');
-const del = require('del');
+const { src, dest, series } = require('gulp');
+const clean = require('gulp-clean');
 const sass = require('gulp-sass')(require('sass'));
-const git = require('gulp-git');
+const git = require('simple-git');
+const cleanCSS = require('gulp-clean-css');
 
-// Tarea para borrar archivos CSS
-gulp.task('borrar-css', function () {
-  return del(['css/*.css']);
-});
+function compilar_Sass(cb) {
+    const archivosSCSS = ['./sass/estilos.scss', './sass/estilos1.scss', './sass/estilos2.scss', './sass/estilos3.scss', './sass/estilos4.scss'];
+    return src(archivosSCSS)
+    .pipe(sass())
+    .pipe(dest('css'));
+}
 
-// Tarea para compilar Sass a CSS y moverlo a la carpeta 'css'
-gulp.task('compilar-sass', function () {
-  return gulp.src('sass/*.scss') // Ruta a tus archivos Sass
-    .pipe(sass({ implementation: require('dart-sass') }).on('error', sass.logError))
-    .pipe(gulp.dest('css'));
-});
+function limpiar_Estilos(cb) {
+    return src('./css/*', { read: false, allowEmpty: false })
+    .pipe(clean());
+}
 
-// Tarea para agregar cambios al área de preparación de Git
-gulp.task('git-add', function () {
-  return gulp.src('.')
-    .pipe(git.add({ maxBuffer: Infinity }));
-});
+function comprimir_CSS(cb) {
+    const archivosCSS = ['./css/estilos.css', './css/estilos1.css', './css/estilos2.css', './css/estilos3.css', './css/estilos4.css'];
+    return src(archivosCSS)
+    .pipe(cleanCSS())
+    .pipe(dest('css'));
+}
 
-// Tarea para realizar un commit en Git
-gulp.task('git-commit', function () {
-  return gulp.src('.')
-    .pipe(git.commit('Actualizar estilos Sass'));
-});
+function subir_GitHub(cb) {
+  git()
+    .add('.')
+    .commit('Subir a GitHub')
+    .push('origin', 'main', function (err) {
+      if (err) {
+        console.error(err);
+        cb(err);
+      } else {
+        cb();
+      }
+    });
+}
 
-// Tarea para realizar un push a GitHub
-gulp.task('git-push', function (cb) {
-  git.push('origin', 'main', function (err) {
-    if (err) return cb(err);
-  });
-});
+exports.limpiarEstilos = limpiar_Estilos;
+exports.compilarSass = compilar_Sass;
+exports.comprimirCSS = comprimir_CSS;
+exports.subirGitHub = subir_GitHub;
 
-// Tarea predeterminada que ejecuta las tareas 'borrar-css', 'compilar-sass' y las tareas de Git en secuencia
-gulp.task('default', gulp.series('borrar-css', 'compilar-sass', 'git-add', 'git-commit', 'git-push'));
+exports.default = series(limpiar_Estilos, compilar_Sass, comprimir_CSS, subir_GitHub);
